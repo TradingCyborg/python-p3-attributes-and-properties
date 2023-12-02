773514ac1d8753671a06f790f4af1a678a36fdba
TradingCyborg <my.github.kinyuad29@gmail.com>
2023-12-02 12:55:29 +0300

    Attributes Work

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,18 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self):
+        self._name = None
+
+    def name(self):
+        return self._name
+    
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value <= 25)):
+            print("Name must be a stringg between 1 and 25 characters.")
+        
+    def breed(self, value):
+        if value not in self.APPROVED_BREEDS:
+            print("Breed must be in the list of approved breeds.")
+        else:
+            self._breed = value 
